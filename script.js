6a93532cd6a6d7f885e38aaf9213f894eedf1af1
rfahlevi <rezafahlevi55@gmail.com>
2020-05-30 12:57:53 +0700

    Js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(window).on('load', function() {
-    $('.kategori .card-kategori, .produk .card-produk').each(function(i) {
-        setTimeout(function() {
-            $('.kategori .card-kategori').eq(i).addClass('show-kategori'),
-            $('.produk .card-produk').eq(i).addClass('show-produk')
-    }, 200 * i);
-    });
-});
+// $(window).on('load', function() {
+//     $('.kategori .card-kategori, .produk .card-produk').each(function(i) {
+//         setTimeout(function() {
+//             $('.kategori .card-kategori').eq(i).addClass('show-kategori'),
+//             $('.produk .card-produk').eq(i).addClass('show-produk')
+//     }, 200 * i);
+//     });
+// });
